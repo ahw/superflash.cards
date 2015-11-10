c3c1321658fe4e1173bac60bb4b265db60ff881a
ahw <andrewhallagan@gmail.com>
2015-11-10 14:36:32 -0800

    Remove Google Analytics pings

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function run() {
   Location.listen(location => {
     route(location.pathname, async (component) => ReactDOM.render(component, container, () => {
       // Track the page view event via Google Analytics
-      window.ga('send', 'pageview');
+      // window.ga('send', 'pageview');
     }));
   });
 }
