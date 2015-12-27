22a790d382520ea6d55b497147dd29d99dd32a8d
ahw <andrewhallagan@gmail.com>
2015-12-27 13:58:58 -0500

    Remove Redux logger

--- store/store.js
+++ store/store.js
@@ -1,15 +1,15 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import createLogger from 'redux-logger'
+// import createLogger from 'redux-logger'
 import rootReducer from '../reducers'
 import * as actions from '../actions'
 import sprintf from 'sprintf'
 
 
-const loggerMiddleware = createLogger()
+// const loggerMiddleware = createLogger()
 const createStoreWithMiddleware = applyMiddleware(
   thunkMiddleware, // lets us dispatch() functions
-  loggerMiddleware // neat middleware that logs actions
+  // loggerMiddleware // neat middleware that logs actions
 )(createStore)
 
 const store = createStoreWithMiddleware(rootReducer)
