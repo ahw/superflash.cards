f2eea30d7ea6cf2a084fc818810d67a2cc38c832
ahw <andrewhallagan@gmail.com>
2019-04-14 20:57:36 -0400

    Recursively print results.children

--- grammar-test.js
+++ grammar-test.js
@@ -1,26 +1,37 @@
 const nearley = require("nearley");
 const grammar = require("./grammar.js");
+const testStrings = require('./grammar-test-strings');
 
-// Create a Parser object from our grammar.
-const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
 // Parse something!
 const blankSpace = '<span style="display:inline-block; width:3em; background:white; border:1px solid black; text-align:center">?</span>';
 const blankSpaceDots = '<span style="display:inline-block; width:3em; background:white; border:1px solid black; text-align:center">?&hellip;?</span>';
 
-let s = "Definition: hello here is ___[somethign]";
-s = 'Just as multiple   factors  shape every system, multiple mental models ___...[from a variety of disciplines] are necessary to understand that system. You have to realize the truth of Julian Huxley\'s idea that, "Life is just ___...[one damn relatedness after another.]"';
-console.log('> ' + s);
-console.log();
+// let s = "Definition: hello here is ___[somethign]";
+// s = 'Just as multiple   factors  shape every system, multiple mental models ___...[from a variety of disciplines] are necessary to understand that system. You have to realize the truth of Julian Huxley\'s idea that, "Life is just ___...[one damn relatedness after another.]"';
+// console.log('> ' + s);
+// console.log();
+
 // parser.feed("Definition: hello here is    ___[somethign]");
-parser.feed(s);
-// parser.results is an array of possible parsings.
-if (parser.results.length > 1) {
-  console.log(`Got multiple parse results (${parser.results.length})`);
-} else {
-  const output = print(parser.results);
-  console.log(output);
-}
-// console.log(JSON.stringify(parser.results, null, '    ')); // [[[[ "foo" ],"\n" ]]]
+testStrings.forEach(line => {
+  // Create a Parser object from our grammar.
+  const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+  console.log('> ' + line);
+  try {
+    parser.feed(line);
+    // parser.results is an array of possible parsings.
+    // console.log(JSON.stringify(parser.results, null, '    '));
+    const output = print(parser.results);
+    console.log(output);
+  } catch (e) { }
+});
+
+// if (parser.results.length > 1) {
+//   console.log(`Got multiple parse results (${parser.results.length})`);
+// } else {
+//   const output = print(parser.results);
+//   console.log(output);
+// }
+// // console.log(JSON.stringify(parser.results, null, '    ')); // [[[[ "foo" ],"\n" ]]]
 
 function print(results) {
   const blanks = [];
@@ -29,6 +40,11 @@ function print(results) {
     if (Array.isArray(results)) {
       // console.log('printing nested array...');
       return results.map(r => _print(r)).join("");
+    }
+
+    if (Array.isArray(results.children)) {
+      // console.log('printing nested array...');
+      return results.value + results.children.map(r => _print(r)).join("");
     }
 
     // console.log(results.type, results.value);
@@ -49,8 +65,9 @@ function print(results) {
     return results.value;
   }
 
-  return {
-    markdown: _print(results),
-    blanks,
-  };
+  return _print(results) + '\n' + blanks.map(b => `  - ${b}`).join('\n');
+  // return {
+  //   markdown: _print(results),
+  //   blanks,
+  // };
 }
--- moo-test.js
+++ moo-test.js
@@ -1,4 +1,5 @@
 const moo = require('moo');
+const testStrings = require('./grammar-test-strings');
 
 // const lexer = moo.compile({
 //   DEFINITION: /Definition: ?/,
@@ -37,20 +38,7 @@ const lexer = moo.compile({
   TEXT_CHAR: /./,
 });
 
-const text = `
-Definition: hello here is    ___[somethign]
-Just as multiple   factors  shape every system, multiple mental models ___...[from a variety of disciplines] are necessary to understand that system. You have to realize the truth of Julian Huxley's idea that, "Life is just ___...[one damn relatedness after another.]"
-What is BlackRock AUM? - $6 trillion
-MMT - MMT, which has been gain­ing sup­port among high-pro­file De­moc­rats, turns con­ven­tional eco­nomic pol­icy on its head. In­stead of leav­ing macro­economic man­age­ment to the in­ter­est-rate-set­ting com­mit­tee of the Fed­eral Re­serve, MMTers be­lieve it is best han­dled by gov­ern­ment spend­ing and tax­a­tion. The most provoca­tive claim of the the­ory is that gov­ern­ment deficits don’t mat­ter in them­selves for coun­tries—such as the U.S.—that bor­row in their own cur­ren­cies.
-Maiden Lane - Maiden Lane Transactions refers to three limited liability companies created by the Federal Reserve Bank of New York in 2008 as a financial vehicle to facilitate transactions involving three entities: the former Bear Stearns company as the first entity, the lending division of the former American International Group (AIG) as the second, and the former AIG's credit default swap division as the third.
-Definition: Annuity - An annuity is a series of payments made at equal intervals.
-Definition: Credit Spread - A credit spread is the difference in yield between a U.S. Treasury bond and another debt security of the same maturity but different credit quality. 
-What is world GDP? - $45 trillion
-How do you measure the liquidity of a stock? - Liquidity can be measured either based on trade volume relative to shares outstanding or based on the bid-ask spread or transactions costs of trading. Money, or cash, is the most liquid asset, because it can be "sold" for goods and services instantly with no loss of value.
-End of the U.S. gold standard? - AUGUST 15, 1971, United States President Richard Nixon announced that foreign-held U.S. dollars would no longer be convertible into gold—thus stripping away the last vestige of the international gold standard.
-`;
-
-text.split('\n').filter(l => l).forEach(line => {
+testStrings.forEach(line => {
   lexer.reset(line);
   console.log(line);
   for(let token of lexer) {
