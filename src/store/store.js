0eb4d2803e104e0c11863ead1cbb778e6e7f6d08
ahw <andrewhallagan@gmail.com>
2019-04-20 10:43:17 -0400

    Fix ESLint errors in store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,16 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-// import createLogger from 'redux-logger'
-import rootReducer from '../reducers'
-import * as actions from '../actions'
-import sprintf from 'sprintf'
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+// import createLogger from 'redux-logger';
+import rootReducer from '../reducers';
+// import * as actions from '../actions';
+// import sprintf from 'sprintf';
 
 
-// const loggerMiddleware = createLogger()
+// const loggerMiddleware = createLogger();
 const createStoreWithMiddleware = applyMiddleware(
   thunkMiddleware, // lets us dispatch() functions
   // loggerMiddleware // neat middleware that logs actions
-)(createStore)
+)(createStore);
 
-const store = createStoreWithMiddleware(rootReducer)
-export default store
+const store = createStoreWithMiddleware(rootReducer);
+export default store;
