352bb5b3750451543fe2c3d9c5a5cebb6780a6ec
ahw <andrewhallagan@gmail.com>
2019-04-19 22:22:31 -0400

    Add grammar-test-strings

---
+++ grammar-test-strings.js
@@ -0,0 +1,22 @@
+const text = `
+Goldman Sachs converted from a privately held partnership into a publicly traded company in xxx(1999). ### companies
+
+Definition: Annuity ??? An annuity is a series of payments made at equal intervals. ### vocab, instruments
+
+Examples of annuities are  1. regular deposits to a xxx(savings account),  2. monthly xxx(home mortgage) payments,  3. monthly xxx(insurance) payments and  4. Xxx(pension) payments. ### vocab, instruments
+The capital of New York is xxx[Albany] ### geography
+A ___[man] a plan, a Canal, Xxx[Panama!] ### history, palendromes
+One fish xxx(two) fish. Red fish, xxx...(blue fish) ### seuss, rhymes
+Definition: hello here is    ___[somethign]
+Just as multiple   factors  shape every system, multiple mental models ___...[from a variety of disciplines] are necessary to understand that system. You have to realize the truth of Julian Huxley's idea that, "Life is just ___...[one damn relatedness after another.]"
+What is BlackRock AUM? ??? $6 trillion ### facts and figures, companies, current events
+MMT ??? MMT, which has been gain­ing sup­port among high-pro­file De­moc­rats, turns con­ven­tional eco­nomic pol­icy on its head. In­stead of leav­ing macro­economic man­age­ment to the in­ter­est-rate-set­ting com­mit­tee of the Fed­eral Re­serve, MMTers be­lieve it is best han­dled by gov­ern­ment spend­ing and tax­a­tion. The most provoca­tive claim of the the­ory is that gov­ern­ment deficits don’t mat­ter in them­selves for coun­tries—such as the U.S.—that bor­row in their own cur­ren­cies.
+Maiden Lane ??? Maiden Lane Transactions refers to three limited liability companies created by the Federal Reserve Bank of New York in 2008 as a financial vehicle to facilitate transactions involving three entities: the former Bear Stearns company as the first entity, the lending division of the former American International Group (AIG) as the second, and the former AIG's credit default swap division as the third.
+Definition: Annuity ??? An annuity is a series of payments made at equal intervals.
+Definition: Credit Spread ??? A credit spread is the difference in yield between a U.S. Treasury bond and another debt security of the same maturity but different credit quality. 
+What is world GDP? ??? $45 trillion
+How do you measure the liquidity of a stock? ??? Liquidity can be measured either based on trade volume relative to shares outstanding or based on the bid-ask spread or transactions costs of trading. Money, or cash, is the most liquid asset, because it can be "sold" for goods and services instantly with no loss of value.
+End of the U.S. gold standard? ??? AUGUST 15, 1971, United States President Richard Nixon announced that foreign-held U.S. dollars would no longer be convertible into gold—thus stripping away the last vestige of the international gold standard.
+`;
+
+module.exports = text.split('\n').filter(l => l);
